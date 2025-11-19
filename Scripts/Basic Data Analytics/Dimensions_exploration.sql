ba47a3b5ce3c0ee3fa4778448b6cc5f07e2ab901
Rahul Gundoju <rahulgundoju777@gmail.com>
2025-11-20 01:57:47 +0530

    Update Dimensions_exploration.sql

--- Scripts/Basic Data Analytics/Dimensions_exploration.sql
+++ Scripts/Basic Data Analytics/Dimensions_exploration.sql
@@ -1,9 +1,27 @@
--- ==================================================
--- Dimensions Exploration
--- ==================================================
--- Explore all countries our customers come from
-Select DISTINCT country from gold.dim_customers
 
--- Explore all categories "The major divisions"
-Select DISTINCT category, subcategory, product_name from gold.dim_products
-ORDER BY 1,2,3
+/*
+===============================================================================
+Dimensions Exploration
+===============================================================================
+Purpose:
+    - To explore the structure of dimension tables.
+	
+SQL Functions Used:
+    - DISTINCT
+    - ORDER BY
+===============================================================================
+*/
+
+-- Retrieve a list of unique countries from which customers originate
+SELECT DISTINCT 
+    country 
+FROM gold.dim_customers
+ORDER BY country;
+
+-- Retrieve a list of unique categories, subcategories, and products
+SELECT DISTINCT 
+    category, 
+    subcategory, 
+    product_name 
+FROM gold.dim_products
+ORDER BY category, subcategory, product_name;
