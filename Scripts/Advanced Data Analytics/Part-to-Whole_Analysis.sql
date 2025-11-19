a981bf538e4acd4e84c2f0b1a903e55f3a27ec0f
Rahul Gundoju <rahulgundoju777@gmail.com>
2025-11-20 02:01:28 +0530

    Update Part-to-Whole_Analysis.sql

--- Scripts/Advanced Data Analytics/Part-to-Whole_Analysis.sql
+++ Scripts/Advanced Data Analytics/Part-to-Whole_Analysis.sql
@@ -1,19 +1,31 @@
---=============================================
--- Part to Whole Analysis
---=============================================
--- Which category contribute the most to overall sales ?
+/*
+===============================================================================
+Part-to-Whole Analysis
+===============================================================================
+Purpose:
+    - To compare performance or metrics across dimensions or time periods.
+    - To evaluate differences between categories.
+    - Useful for A/B testing or regional comparisons.
+
+SQL Functions Used:
+    - SUM(), AVG(): Aggregates values for comparison.
+    - Window Functions: SUM() OVER() for total calculations.
+===============================================================================
+*/
+-- Which categories contribute the most to overall sales?
 WITH category_sales AS (
-Select 
-category,
-SUM(sales_amount) total_sales
-from gold.fact_sales F
-LEFT JOIN gold.dim_products p
-ON p.product_key = f.product_key
-GROUP BY category)
-Select 
-category,
-total_sales,
-SUM(total_sales) OVER() overall_sales,
-CONCAT(ROUND((CAST(total_sales as FLOAT)/SUM(total_sales) OVER())*100, 2), '%') AS percentage_of_total 
-from category_sales
-ORDER BY total_sales DESC
+    SELECT
+        p.category,
+        SUM(f.sales_amount) AS total_sales
+    FROM gold.fact_sales f
+    LEFT JOIN gold.dim_products p
+        ON p.product_key = f.product_key
+    GROUP BY p.category
+)
+SELECT
+    category,
+    total_sales,
+    SUM(total_sales) OVER () AS overall_sales,
+    ROUND((CAST(total_sales AS FLOAT) / SUM(total_sales) OVER ()) * 100, 2) AS percentage_of_total
+FROM category_sales
+ORDER BY total_sales DESC;
