35a93f9db60de6f70a8f73fdd2a8924b19f74755
Rahul Gundoju <rahulgundoju777@gmail.com>
2025-11-20 02:02:15 +0530

    Update Products_Report.sql

--- Scripts/Advanced Data Analytics/Products_Report.sql
+++ Scripts/Advanced Data Analytics/Products_Report.sql
@@ -1,101 +1,114 @@
---=============================================
--- Build Product report
---=============================================
-/* 
---=============================================
--- Products Report
---=============================================
+/*
+===============================================================================
+Product Report
+===============================================================================
 Purpose:
-	- This report consolidates key product metrics and behaviours
+    - This report consolidates key product metrics and behaviors.
+
 Highlights:
-	1. Gathers essential fields such as product names, category, sub-category and cost
-    2. Segments products by revenue to identify High-performers, Mid-range, or Low-performers.
+    1. Gathers essential fields such as product name, category, subcategory, and cost.
+    2. Segments products by revenue to identify High-Performers, Mid-Range, or Low-Performers.
     3. Aggregates product-level metrics:
-		- Total orders
-        - Total sales
-        - Total Quantity sold
-        - Total customers(unique)
-        - Lifespan (in months)
-	4. Calculates valuable KPIs:
-		- recency (months since last sale)
-        - average order revenue(AOR)
-        - average monthly revenue
---=============================================
+       - total orders
+       - total sales
+       - total quantity sold
+       - total customers (unique)
+       - lifespan (in months)
+    4. Calculates valuable KPIs:
+       - recency (months since last sale)
+       - average order revenue (AOR)
+       - average monthly revenue
+===============================================================================
 */
+-- =============================================================================
+-- Create Report: gold.report_products
+-- =============================================================================
+IF OBJECT_ID('gold.report_products', 'V') IS NOT NULL
+    DROP VIEW gold.report_products;
+GO
+
 CREATE VIEW gold.report_products AS
-/*---------------------------------------------
-1) Basic Query: Retrieves core columns from fact_sales and dim_products
------------------------------------------------*/
-WITH base_query AS
-(
-	Select 
-		f.order_number,
-		f.order_date,
+
+WITH base_query AS (
+/*---------------------------------------------------------------------------
+1) Base Query: Retrieves core columns from fact_sales and dim_products
+---------------------------------------------------------------------------*/
+    SELECT
+	    f.order_number,
+        f.order_date,
 		f.customer_key,
-		f.sales_amount,
-		f.quantity,
-		p.product_key,
-		p.product_name,
-		p.category,
-		p.subcategory,
-		p.cost
-    from gold.fact_sales f
+        f.sales_amount,
+        f.quantity,
+        p.product_key,
+        p.product_name,
+        p.category,
+        p.subcategory,
+        p.cost
+    FROM gold.fact_sales f
     LEFT JOIN gold.dim_products p
-    ON f.product_key = p.product_key
-    WHERE order_date IS NOT NULL -- only consider valid sales dates
-)
-/*--------------------------------------------------------------------
-2) Customer aggregations: Summarizes key metrics at the customer level
---------------------------------------------------------------------*/ 
-, product_aggregations as (
-select
-	product_key,
+        ON f.product_key = p.product_key
+    WHERE order_date IS NOT NULL  -- only consider valid sales dates
+),
+
+product_aggregations AS (
+/*---------------------------------------------------------------------------
+2) Product Aggregations: Summarizes key metrics at the product level
+---------------------------------------------------------------------------*/
+SELECT
+    product_key,
     product_name,
     category,
     subcategory,
     cost,
-    DATEDIFF(month, MIN(order_date), MAX(order_date)) as lifespan,
-    MAX(order_date) as last_order_date,
-    COUNT(DISTINCT order_number) as total_orders,
-    COUNT(DISTINCT customer_key) as total_customers,
-    SUM(sales_amounts) as total_sales,
-    SUM(quantity) as total_quantity,
-    ROUND(AVG(CAST(sales_amounts as float)/NULLIF(quantity,0)),1) as avg_selling_price
-from base_query
+    DATEDIFF(MONTH, MIN(order_date), MAX(order_date)) AS lifespan,
+    MAX(order_date) AS last_sale_date,
+    COUNT(DISTINCT order_number) AS total_orders,
+	COUNT(DISTINCT customer_key) AS total_customers,
+    SUM(sales_amount) AS total_sales,
+    SUM(quantity) AS total_quantity,
+	ROUND(AVG(CAST(sales_amount AS FLOAT) / NULLIF(quantity, 0)),1) AS avg_selling_price
+FROM base_query
+
 GROUP BY
-	product_key,
+    product_key,
     product_name,
     category,
     subcategory,
-    cost,
+    cost
 )
-/*--------------------------------------------------------------------
-3) Final Query: Combine all the products results into on output
---------------------------------------------------------------------*/ 
-select 
+
+/*---------------------------------------------------------------------------
+  3) Final Query: Combines all product results into one output
+---------------------------------------------------------------------------*/
+SELECT 
 	product_key,
-    product_name,
-    category,
-    subcategory,
-    cost,
-    last_sale_date,
-    DATEDIFF(month, last_sale_date, GETDATE()) as recency_in_months,
-    CASE WHEN total_sales > 50000 THEN 'High-performer'
-		 WHEN total_sales >= 10000 THEN 'Mid-Range'
-         ELSE 'Low_performer'
-	END as product_segment,
-    lifespan,
-    total_orders,
-    total_sales,
-    total_quantity,
-    total_customers,
-    avg_selling_price,
-    -- Avg order revenue(AOR)
-    CASE WHEN total_orders = 0 THEN 0
-		 ELSE total_sales/total_orders
-	END as avg_order_revenue,
-    -- Avg monthly revenue
-    CASE WHEN lifespan = 0 THEN total_sales,
-		 ELSE total_sales/lifespan
-	END as avg_monthly_revenue
-from product_aggregations
+	product_name,
+	category,
+	subcategory,
+	cost,
+	last_sale_date,
+	DATEDIFF(MONTH, last_sale_date, GETDATE()) AS recency_in_months,
+	CASE
+		WHEN total_sales > 50000 THEN 'High-Performer'
+		WHEN total_sales >= 10000 THEN 'Mid-Range'
+		ELSE 'Low-Performer'
+	END AS product_segment,
+	lifespan,
+	total_orders,
+	total_sales,
+	total_quantity,
+	total_customers,
+	avg_selling_price,
+	-- Average Order Revenue (AOR)
+	CASE 
+		WHEN total_orders = 0 THEN 0
+		ELSE total_sales / total_orders
+	END AS avg_order_revenue,
+
+	-- Average Monthly Revenue
+	CASE
+		WHEN lifespan = 0 THEN total_sales
+		ELSE total_sales / lifespan
+	END AS avg_monthly_revenue
+
+FROM product_aggregations 
