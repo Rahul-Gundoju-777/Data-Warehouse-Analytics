2d6fe412d322bae609f5ff1c5299aa271611221f
Rahul Gundoju <rahulgundoju777@gmail.com>
2025-11-20 01:58:21 +0530

    Update Date_exploration.sql

--- Scripts/Basic Data Analytics/Date_exploration.sql
+++ Scripts/Basic Data Analytics/Date_exploration.sql
@@ -1,19 +1,27 @@
--- ==================================================
--- Date Exploration
--- ==================================================
--- Find the Date of first and last order
--- How many years of sales are available
-select 
-MIN(order_date) first_order_date,
-MAX(order_date) last_order_date,
-DATEDIFF(year, MIN(order_date), MAX(order_date)) as order_range_years,
-DATEDIFF(month, MIN(order_date), MAX(order_date)) as order_range_month
-from gold.fact_sales
+/*
+===============================================================================
+Date Exploration 
+===============================================================================
+Purpose:
+    - To determine the temporal boundaries of key data points.
+    - To understand the range of historical data.
 
--- Find the youngest and oldest customers
-select 
-MIN(birthdate) as oldest_birthdate,
-MAX(birthdate) as youngest_birthdate,
-DATEDIFF(year, MIN(birthdate), GETDATE()) as oldest_age,
-DATEDIFF(year, MAX(birthdate), GETDATE()) as youngest_age
-from gold.dim_customers
+SQL Functions Used:
+    - MIN(), MAX(), DATEDIFF()
+===============================================================================
+*/
+
+-- Determine the first and last order date and the total duration in months
+SELECT 
+    MIN(order_date) AS first_order_date,
+    MAX(order_date) AS last_order_date,
+    DATEDIFF(MONTH, MIN(order_date), MAX(order_date)) AS order_range_months
+FROM gold.fact_sales;
+
+-- Find the youngest and oldest customer based on birthdate
+SELECT
+    MIN(birthdate) AS oldest_birthdate,
+    DATEDIFF(YEAR, MIN(birthdate), GETDATE()) AS oldest_age,
+    MAX(birthdate) AS youngest_birthdate,
+    DATEDIFF(YEAR, MAX(birthdate), GETDATE()) AS youngest_age
+FROM gold.dim_customers;
